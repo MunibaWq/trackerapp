0353262521f64277834e720171510442f22d36e1
Muniba Waqar <muniba.wq@gmail.com>
2021-09-06 11:05:10 -0600

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="mainBody">
+      <h3> IP Address Tracker</h3>
+    </div>
+  );
+};
+
+export { Header };
