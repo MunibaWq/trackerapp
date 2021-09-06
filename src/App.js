e47443f48a4391f5849ddca77c53f0422a85a939
Muniba Waqar <muniba.wq@gmail.com>
2021-09-06 10:52:38 -0600

    Setup site wrapper and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Footer from "./components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* Header block element */}
+      {/* Search results on top of the Header and Body Elements */}
+      {/* Body Block element */}
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      IP Address Tracker Search for any IP address or domain IP Address Location
+      Timezone UTC
+      {/* <!-- add offset value dynamically using the API --> */}
+      ISP
+      <div className="attribution">
+        Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0/hub/ip-address-tracker-6uWB1SBT7"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by
+        <a href="https://github.com/MunibaWq/trackerapp">Muniba Waqar</a>.
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
