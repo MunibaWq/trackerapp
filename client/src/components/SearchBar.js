f405b200b4687e64d113780a483f61c75ca6127a
Muniba Waqar <muniba.wq@gmail.com>
2021-09-07 11:32:59 -0600

    style searchbar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "./SearchBar.css";
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log("form submitted");
+};
 
 const SearchBar = () => {
   return (
-    <>
+    <form className="searchForm" onSubmit={handleSubmit}>
       <input type="text" placeholder="Search for any IP address or domain" />
-    </>
+      <button className="searchBtn" type="submit">
+        <img src="../images/icon-arrow.svg" alt="" />
+      </button>
+    </form>
   );
 };
 
