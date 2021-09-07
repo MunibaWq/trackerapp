376a854491e4939eceab3fe08ea5d4be724768f3
Muniba Waqar <muniba.wq@gmail.com>
2021-09-07 13:55:06 -0600

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,23 +2,17 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      IP Address Tracker Search for any IP address or domain IP Address Location
-      Timezone UTC
-      {/* <!-- add offset value dynamically using the API --> */}
-      ISP
-      <div className="attribution">
-        Challenge by
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0"
-        >
-          {" "}
-          Frontend Mentor
-        </a>
-        . Coded by{" "}
-        <a href="https://github.com/MunibaWq/trackerapp">Muniba Waqar</a>.
-      </div>
+    <div className="attribution">
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0"
+      >
+        {" "}
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="https://github.com/MunibaWq/trackerapp">Muniba Waqar</a>.
     </div>
   );
 };
