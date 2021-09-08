a6fcf7f012782504c7715ad443e8ed8726a14924
Muniba Waqar <muniba.wq@gmail.com>
2021-09-07 22:35:41 -0600

    style card content

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,22 +4,22 @@ import "./Card.css";
 const Card = () => {
   return (
     <div className="container">
-      <div>
+      <div className="padding">
         <h5>IP Address</h5>
         <p>IP Address goes here</p>
       </div>
       <hr />
-      <div>
+      <div className="padding">
         <h5>Location</h5>
         <p>Location goes here</p>
       </div>
       <hr />
-      <div>
+      <div className="padding">
         <h5>Time Zone</h5>
         <p>Time Zone goes here</p>
       </div>
       <hr />
-      <div>
+      <div className="padding">
         <h5>ISP</h5>
         <p>ISP goes here</p>
       </div>
