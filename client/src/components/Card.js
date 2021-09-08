2325bbe9264e8764300183eb8fd7c4228b9cbdad
Muniba Waqar <muniba.wq@gmail.com>
2021-09-07 22:32:34 -0600

    add card content

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./Card.css";
 
 const Card = () => {
-  return <div className="container"> Card</div>;
+  return (
+    <div className="container">
+      <div>
+        <h5>IP Address</h5>
+        <p>IP Address goes here</p>
+      </div>
+      <hr />
+      <div>
+        <h5>Location</h5>
+        <p>Location goes here</p>
+      </div>
+      <hr />
+      <div>
+        <h5>Time Zone</h5>
+        <p>Time Zone goes here</p>
+      </div>
+      <hr />
+      <div>
+        <h5>ISP</h5>
+        <p>ISP goes here</p>
+      </div>
+    </div>
+  );
 };
 
 export { Card };
